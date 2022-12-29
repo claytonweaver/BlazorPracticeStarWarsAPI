c87bb6fd0821cdc192ed87a4ec4713d647bfb60f
Clayton Weaver <cweaver@cbctrenton.com>
2022-12-28 22:00:26 -0500

    first

---
+++ Models/Planet.cs
@@ -0,0 +1,6 @@
+namespace StarWarsUI.Models;
+
+public class Planet
+{
+    public string? Name { get; set; }
+}
---
+++ Models/PlanetResponse.cs
@@ -0,0 +1,6 @@
+namespace StarWarsUI.Models;
+
+public class PlanetResponse
+{
+    public IEnumerable<Planet>? Results { get; set; }
+}
